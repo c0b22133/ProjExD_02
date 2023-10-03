8d8197b6f2759e10703a3c0e628754ab057b95d3
mashino <c0b2213326@edu.teu.ac.jp>
2023-10-03 17:58:28 +0900

    チェック２ #1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import random
 import sys
 import pygame as pg
 WIDTH, HEIGHT = 1600, 900
+
 delta = {  # 練習３：移動量辞書
     pg.K_UP: (0, -5),
     pg.K_DOWN: (0, +5),
@@ -9,6 +10,7 @@ delta = {  # 練習３：移動量辞書
     pg.K_RIGHT: (+5, 0),
 }
 accs = [a for a in range(1,11)]
+
 
 def check_bound(obj_rct: pg.Rect):
     """
@@ -22,6 +24,8 @@ def check_bound(obj_rct: pg.Rect):
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom: # 縦方向判定
         tate = False
     return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -42,25 +46,22 @@ def main():
     vx, vy = +5, +5  # 練習２：爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
 
-
         if kk_rct.colliderect(bd_rct):  # 練習５：ぶつかってたら
-            screen.blit(naki_img, bd_rct)
-            pg.display.update()
             print("ゲームオーバー")
+            screen.blit(bg_img, [0, 0])
+            screen.blit(naki_img, kk_rct)
+            pg.display.update()
             return
 
-
-
-
         screen.blit(bg_img, [0, 0])
-
         avx, avy = vx*accs[min(tmr//500, 9)],vy*accs[min(tmr//500, 9)]
-
         """こうかとん"""
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
@@ -84,6 +85,8 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
+
+
 if __name__ == "__main__":
     pg.init()
     main()
